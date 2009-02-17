3c947d28fdcd09634fe313714de5f9e74da69274
Dave Briccetti <daveb@davebsoft.com>
2009-02-16 23:32:19 -0800

    Show friends’ and public statuses in a tabbed pane. (Still needs some refactoring and cleanup.)

--- twitter-client/src/com/davebsoft/sctw/Main.scala
+++ twitter-client/src/com/davebsoft/sctw/Main.scala
@@ -9,62 +9,89 @@ import scala.swing._
 import scala.xml._
 
 /**
- * Continually displays current Twitter public timeline statuses in a Swing JTable.
- * Written for the purposes of learning Scala and the Twitter API. Your feedback is welcome!
- * I want to know every little thing that should be improved.
+ * Continually displays Twitter statuses in a Swing JTable.
+ * Written for the purposes of learning Scala and the Twitter API, probably not
+ * to create a real Twitter client.
+ * 
+ * Your feedback is welcome!
  * 
  * @Author Dave Briccetti, daveb@davebsoft.com, @dcbriccetti
  */
 object Main extends SimpleGUIApplication {
   
-  /** Statuses, in a list for direct access from table model */
-  val statuses = Collections.synchronizedList(new ArrayList[Node]())
-  
-  val statusDataProvider = new StatusDataProvider
-  
+  val friendsStatuses = Collections.synchronizedList(new ArrayList[Node]())
+  var friendsModel: AbstractTableModel = null
+
+  val publicStatuses = Collections.synchronizedList(new ArrayList[Node]())
   var publicModel: AbstractTableModel = null
 
   /** How often, in ms, to fetch and load new data */
-  private final var RELOAD_INTERVAL = 10000;
-
-  statusDataProvider.loadTwitterData(statuses)
+  private final var RELOAD_INTERVAL = 120 * 1000;
+  
+  private var username: String = null
+  private var password: String = null
 
   /**
-   * Creates the Swing frame, which consists of a JTable inside a JScrollPane.
+   * Creates the Swing frame, which consists of a TabbedPane with two panes, 
+   * with each pane containing a JTable inside a JScrollPane.
    */
   def top = {
+    val friendsStatusDataProvider = new FriendsStatusDataProvider(username, password)
+    val publicStatusDataProvider = new PublicStatusDataProvider()
+  
     new MainFrame {
       title = "Too-Simple Twitter Client"
-      val tp: TabbedPane = new TabbedPane() {
+      
+      contents = new TabbedPane() {
+        preferredSize = new Dimension(600, 600)
+        pages.append(new TabbedPane.Page("Friends", new ScrollPane {
+          val table = createTable(friendsStatuses)
+          friendsModel = table.model.asInstanceOf[AbstractTableModel]
+          contents = table
+        }))
         pages.append(new TabbedPane.Page("Public", new ScrollPane {
-          preferredSize = new Dimension(600, 600)
-          contents = new Table() {
-            model = new StatusTableModel(statuses)
-            publicModel = model.asInstanceOf[AbstractTableModel]
-            val colModel = peer.getColumnModel
-            colModel.getColumn(0).setPreferredWidth(100)
-            colModel.getColumn(1).setPreferredWidth(500)
-          }
+          val table = createTable(publicStatuses)
+          publicModel = table.model.asInstanceOf[AbstractTableModel]
+          contents = table
         }))
       }
-      contents = tp
+      
       peer.setLocationRelativeTo(null)
 
-      continuallyLoadData(this)
+      friendsStatusDataProvider.loadTwitterData(friendsStatuses)
+      publicStatusDataProvider.loadTwitterData(publicStatuses)
+
+      continuallyLoadData(friendsStatusDataProvider, friendsStatuses, friendsModel)
+      continuallyLoadData(publicStatusDataProvider, publicStatuses, publicModel)
+    }
+  }
+    
+  private def createTable(statuses: java.util.List[Node]): Table = {
+    new Table() {
+      model = new StatusTableModel(statuses)
+      val colModel = peer.getColumnModel
+      colModel.getColumn(0).setPreferredWidth(100)
+      colModel.getColumn(1).setPreferredWidth(500)
     }
   }
 
-  /**
-   * Reloads the data periodically
-   */
-  private def continuallyLoadData(container: Container) {
+  private def continuallyLoadData(statusDataProvider: StatusDataProvider,
+      statuses: java.util.List[Node], model: AbstractTableModel) {
     new Timer(RELOAD_INTERVAL, new ActionListener() {
       def actionPerformed(event: ActionEvent) {
         statusDataProvider.loadTwitterData(statuses)
-        publicModel.fireTableDataChanged
+        model.fireTableDataChanged
       }
     }).start
   }
 
-  override def main(args: Array[String]): Unit = super.main(args)  // Without this, IDEA doesn’t see main 
+  override def main(args: Array[String]): Unit = {
+    if (args.length >= 2) {
+      username = args(0)
+      password = args(1)
+      super.main(args)
+    } else {
+      println("Missing username and password")
+    }
+  }
 }
--- twitter-client/src/com/davebsoft/sctw/StatusDataProvider.scala
+++ twitter-client/src/com/davebsoft/sctw/StatusDataProvider.scala
@@ -1,24 +1,37 @@
 package com.davebsoft.sctw
 
+import org.apache.commons.httpclient.auth.AuthScope
+import org.apache.commons.httpclient.{UsernamePasswordCredentials, HttpClient}
 import scala.xml._
 import javax.swing.table.AbstractTableModel
-import org.apache.commons.httpclient.HttpClient
 import org.apache.commons.httpclient.methods.GetMethod
 
-class StatusDataProvider {
+abstract class StatusDataProvider() {
   val httpClient = new HttpClient()
-  val timelineMethod = new GetMethod("http://twitter.com/statuses/public_timeline.xml")
+  def getUrl: String
+  val method = new GetMethod(getUrl)
   
   /**
-   * Fetches statuses from Twitter and stores them in statuses field.
+   * Fetches statuses from Twitter and stores them in <code>statuses</code>.
    */
   def loadTwitterData(statuses: java.util.List[Node]) {
-    val result = httpClient.executeMethod(timelineMethod)
-    val timeline = XML.load(timelineMethod.getResponseBodyAsStream())
+    val result = httpClient.executeMethod(method)
+    val timeline = XML.load(method.getResponseBodyAsStream())
+    method.releaseConnection
     statuses.clear
     for (st <- timeline \\ "status") {
       statuses.add(st)
     }
   }
+}
+
+class PublicStatusDataProvider() extends StatusDataProvider {
+  def getUrl() = "http://twitter.com/statuses/public_timeline.xml"
+}
+
+class FriendsStatusDataProvider(username: String, password: String) extends StatusDataProvider {
+  httpClient.getState().setCredentials(new AuthScope("twitter.com", 80, AuthScope.ANY_REALM), 
+    new UsernamePasswordCredentials(username, password));
   
+  def getUrl() = "http://twitter.com/statuses/friends_timeline.xml"
 }
