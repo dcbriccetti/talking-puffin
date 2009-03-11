87cefe7717bd9ed5a1ebe3f986564840d7882605
Dave Briccetti <daveb@davebsoft.com>
2009-03-11 09:12:04 -0700

    Formatting, minor changes

--- twitter-client/src/main/scala/com/davebsoft/sctw/ui/AgeCellRenderer.scala
+++ twitter-client/src/main/scala/com/davebsoft/sctw/ui/AgeCellRenderer.scala
@@ -15,16 +15,14 @@ class AgeCellRenderer extends DefaultTableCellRenderer {
     val mins = (time / 60) - (days * 1440) - (hours * 60)
     val seconds = time % 60
     val sb = new StringBuilder
-    if (days > 0) sb.append(twoDigitNum(days)).append(":")
+    if (days > 0)                   sb.append(twoDigitNum(days)) .append(":")
     if (hours > 0 || sb.length > 0) sb.append(twoDigitNum(hours)).append(":")
-    if (mins > 0 || sb.length > 0) sb.append(twoDigitNum(mins)).append(":")
+    if (mins > 0  || sb.length > 0) sb.append(twoDigitNum(mins)) .append(":")
     sb.append(twoDigitNum(seconds))
     setText(sb.toString)
   }
 
-  private def twoDigitNum(num: java.lang.Long): String = {
-    String.format("%02d", num)
-  }
+  private def twoDigitNum(num: java.lang.Long): String = String.format("%02d", num)
   
 }
 
