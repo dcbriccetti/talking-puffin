d12abebe40f73b42833eb6abe22b8feed695ba43
Dave Briccetti <daveb@davebsoft.com>
2009-03-12 23:20:08 -0700

    Add comma and period to the class of characters not in a @user name

--- twitter-client/src/main/scala/com/davebsoft/sctw/ui/HtmlFormatter.scala
+++ twitter-client/src/main/scala/com/davebsoft/sctw/ui/HtmlFormatter.scala
@@ -9,13 +9,17 @@ import com.davebsoft.sctw.twitter.Utils
 object HtmlFormatter {
 
   def createTweetHtml(text: String, replyTo: String): String = {
-    val replyToUser = Utils.getReplyToUser(text)   
-    val parent = if (replyTo.length > 0 && replyToUser.length > 0) 
+    val replyToUser = Utils.getReplyToUser(text)
+    
+    val arrowLinkToParent = if (replyTo.length > 0 && replyToUser.length > 0) 
       "<a href='http://twitter.com/" + replyToUser + 
       "/statuses/" + replyTo + "'>↑</a> " else "" 
+    
     var r = text.replaceAll("""(https?://[^'"\s]+)""", "<a href='$1'>$1</a>")
-    r = r.replaceAll("""@([^\s:]+)""", "<a href='http://twitter.com/$1'>@$1</a>")
-    "<html>" + parent + "<font face='Georgia' size='+2'>" + r + "</font></html>"    
+    
+    r = r.replaceAll("""@([^\s:.,]+)""", "<a href='http://twitter.com/$1'>@$1</a>")
+    
+    "<html>" + arrowLinkToParent + "<font face='Georgia' size='+2'>" + r + "</font></html>"    
   }
 
 }
