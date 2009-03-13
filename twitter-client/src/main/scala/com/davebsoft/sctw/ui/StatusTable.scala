ce14b3210f2437397db484c405162bb357a475ae
Dave Briccetti <daveb@davebsoft.com>
2009-03-13 01:16:37 -0700

    Map n and p keys to next and previous movements in table

--- twitter-client/src/main/scala/com/davebsoft/sctw/ui/StatusTable.scala
+++ twitter-client/src/main/scala/com/davebsoft/sctw/ui/StatusTable.scala
@@ -4,12 +4,12 @@ import _root_.com.davebsoft.sctw.util.PopupListener
 import _root_.scala.swing.event.ButtonClicked
 import _root_.scala.xml.{NodeSeq, Node}
 
-import java.awt.event.{MouseEvent, ActionEvent, MouseAdapter, ActionListener}
+import java.awt.{Desktop}
+import java.awt.event.{KeyEvent, ActionEvent, ActionListener, MouseEvent, MouseAdapter}
 import java.awt.image.BufferedImage
-import java.awt.{Color, Desktop, Dimension, Insets, Font}
 import java.net.{URI, URL}
 import java.util.Comparator
-import javax.swing._
+import javax.swing.{JTable, JMenu, JMenuItem, JPopupMenu, KeyStroke}
 import javax.swing.event._
 import javax.swing.table.{DefaultTableCellRenderer, TableRowSorter, TableCellRenderer}
 import scala.swing._
@@ -38,15 +38,31 @@ class StatusTable(statusTableModel: StatusTableModel, statusSelected: (NodeSeq) 
   val statusCol = colModel.getColumn(2)
   statusCol.setPreferredWidth(600)
 
+  val muteAction = Action("Mute") {statusTableModel.muteSelectedUsers(getSelectedModelIndexes)}
+  getActionMap.put(muteAction.title, muteAction.peer)
+  getInputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_M, 0), muteAction.title)
+  
+  val nextAction = Action("Next") {
+    dispatchEvent(new KeyEvent(this, KeyEvent.KEY_PRESSED, System.currentTimeMillis, 
+      0, KeyEvent.VK_DOWN, KeyEvent.CHAR_UNDEFINED))
+  }
+  getActionMap.put(nextAction.title, nextAction.peer)
+  getInputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_N, 0), nextAction.title)
+  
+  val prevAction = Action("Previous") {
+    dispatchEvent(new KeyEvent(this, KeyEvent.KEY_PRESSED, System.currentTimeMillis, 
+      0, KeyEvent.VK_UP, KeyEvent.CHAR_UNDEFINED))
+  }
+  getActionMap.put(prevAction.title, prevAction.peer)
+  getInputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_P, 0), prevAction.title)
+  
   addMouseListener(new PopupListener(this, getPopupMenu))
   addMouseListener(new MouseAdapter {
     override def mouseClicked(e: MouseEvent) = {
       if (e.getClickCount == 2) {
-        val status = statusTableModel.getStatusAt(convertRowIndexToModel(
-          getSelectedRow))
+        val status = statusTableModel.getStatusAt(convertRowIndexToModel(getSelectedRow))
         var uri = "http://twitter.com/" +
-                (status \ "user" \ "screen_name").text + "/statuses/" +
-                (status \ "id").text
+          (status \ "user" \ "screen_name").text + "/statuses/" + (status \ "id").text
         if (Desktop.isDesktopSupported) {
           Desktop.getDesktop.browse(new URI(uri))
         }
@@ -67,13 +83,8 @@ class StatusTable(statusTableModel: StatusTableModel, statusSelected: (NodeSeq) 
   def getPopupMenu: JPopupMenu = {
     val menu = new JPopupMenu()
 
-    val mi = new JMenuItem("Mute")
-    mi.addActionListener(new ActionListener() {
-      def actionPerformed(e: ActionEvent) = {
-        statusTableModel.muteSelectedUsers(getSelectedModelIndexes)
-      }
-    })
-    menu.add(mi)
+    val mi = new MenuItem(muteAction)
+    menu.add(mi.peer)
 
     val tagAl = new ActionListener() {
       def actionPerformed(e: ActionEvent) = {
