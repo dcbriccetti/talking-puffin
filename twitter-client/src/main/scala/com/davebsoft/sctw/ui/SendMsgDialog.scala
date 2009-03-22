344bf53f738ab3567e91a5eff7f017dcb70a95c1
Dave Briccetti <daveb@davebsoft.com>
2009-03-22 16:03:34 -0700

    Show the number of characters remaining in the tweet being composed

--- twitter-client/src/main/scala/com/davebsoft/sctw/ui/SendMsgDialog.scala
+++ twitter-client/src/main/scala/com/davebsoft/sctw/ui/SendMsgDialog.scala
@@ -1,10 +1,12 @@
 package com.davebsoft.sctw.ui
 
+import _root_.scala.actors.Reaction
 import _root_.scala.swing._
-import _root_.scala.swing.event.EditDone
+import _root_.scala.swing.event.{CaretUpdate, EditDone}
 import _root_.scala.xml.NodeSeq
-import java.awt.Dimension
 import java.awt.event.{KeyAdapter, KeyEvent}
+import java.awt.{Dimension, Font}
+import java.net.URLEncoder
 import javax.swing.KeyStroke
 import twitter.Sender
 
@@ -15,9 +17,16 @@ import twitter.Sender
 
 class SendMsgDialog(parent: java.awt.Component, sender: Sender, status: Option[NodeSeq]) extends Frame {
   class CustomTextArea extends TextArea { 
-    preferredSize = new Dimension(500, 100); wordWrap = true; lineWrap = true}
-  
+    preferredSize = new Dimension(400, 80); wordWrap = true; lineWrap = true
+    font = new Font(font.getFontName, Font.BOLD, font.getSize * 150 / 100)
+  }
+
+  val total = new Label
   val message = new CustomTextArea
+  listenTo(message.caret)
+  reactions += {
+    case CaretUpdate(c) => total.text = "Characters remaining: " + (140 - message.text.length) 
+  }
   message.peer.addKeyListener(new KeyAdapter() {
     override def keyTyped(e: KeyEvent) = {
       if (e.getKeyChar == '\n') send 
@@ -39,6 +48,7 @@ class SendMsgDialog(parent: java.awt.Component, sender: Sender, status: Option[N
       case None =>
     }
     add(message, new Constr {grid=(0,0); fill=GridBagPanel.Fill.Both; weightx=1; weighty=1})
+    add(total,   new Constr {grid=(0,1); anchor=GridBagPanel.Anchor.West})
   }
   pack
   peer.setLocationRelativeTo(parent)
