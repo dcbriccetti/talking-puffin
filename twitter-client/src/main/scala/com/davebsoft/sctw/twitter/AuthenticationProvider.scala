5a5a215270a35f29e948650d4ed17a308193c1ed
Dave Briccetti <daveb@davebsoft.com>
2009-04-02 00:01:03 -0700

    Don't display status code if 200

--- twitter-client/src/main/scala/com/davebsoft/sctw/twitter/AuthenticationProvider.scala
+++ twitter-client/src/main/scala/com/davebsoft/sctw/twitter/AuthenticationProvider.scala
@@ -2,11 +2,12 @@ package com.davebsoft.sctw.twitter
 
 class AuthenticationProvider extends HttpHandler {
 
-  def userAuthenticates(userName: String, password: String) = {
+  def userAuthenticates(userName: String, password: String): Boolean = {
     setCredentials(userName, password)
     val (method, result, body) = doGet("http://twitter.com/account/verify_credentials.json")
+    if (result == 200) return true
     println(result)
-    result == 200
+    false
   }
 
 }
