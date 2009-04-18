339fa53d519a682feb929e6217275d0709e71663
Dave Briccetti <daveb@davebsoft.com>
2009-04-18 00:02:12 -0700

    Continue adding support for multiple streams

--- src/main/scala/com/davebsoft/sctw/Main.scala
+++ src/main/scala/com/davebsoft/sctw/Main.scala
@@ -3,8 +3,8 @@ package com.davebsoft.sctw
 import _root_.scala.swing.event.{ButtonClicked, SelectionChanged, WindowClosing}
 import filter.{FilterSet, TagUsers}
 import java.awt.event.{ActionEvent, ActionListener, KeyEvent}
-import java.awt.{Dimension}
-import javax.swing.UIManager
+import java.awt.{Dimension, BorderLayout}
+import javax.swing.{JToolBar, UIManager, JFrame}
 import scala.swing._
 import scala.xml._
 import TabbedPane._
@@ -28,7 +28,7 @@ object Main extends GUIApplication {
   /**
    * Creates the Swing frame.
    */
-  def top = {
+  def createTopFrame = {
 
     val tweetsProvider = new TweetsProvider(username, password, StateRepository.get("highestId", null))
     val repliesProvider = new RepliesProvider(username, password)
@@ -40,27 +40,52 @@ object Main extends GUIApplication {
     val repliesFilterSet = new FilterSet
     val repliesModel = new StatusTableModel(new StatusTableOptions(false), repliesProvider, 
       usersModel, repliesFilterSet, username) with Replies
-    val statusPane  = new ToolbarStatusPane(tweetsTitle, tweetsModel,  apiHandlers, tweetsFilterSet)
+    val statusPane  = new ToolbarStatusPane(tweetsTitle,  tweetsModel,  apiHandlers, tweetsFilterSet)
     val repliesPane = new RepliesStatusPane(repliesTitle, repliesModel, apiHandlers, repliesFilterSet) {
       table.showColumn(3, false)
     }
+    var streams = List[StreamInfo]()
+    
     val tabbedPane = new TabbedPane() {
       preferredSize = new Dimension(900, 600)
-
-      pages.append(new Page(tweetsTitle, statusPane))
-      pages.append(new Page(repliesTitle, repliesPane))
-
+      pages += new Page(tweetsTitle, statusPane)
+      streams ::= new StreamInfo(tweetsTitle, tweetsModel, statusPane)
+      pages += new Page(repliesTitle, repliesPane)
+      streams ::= new StreamInfo(repliesTitle, repliesModel, repliesPane)
     }
 
     val clearAction = statusPane.clearAction
     val frame = new Frame {
       title = "Simple Twitter Client"
+      val newFrame = this
 
       TagUsers.load
 
       listenTo(tweetsModel)
       listenTo(repliesModel)
-      contents = tabbedPane
+      contents = new BorderPanel {
+        val toolBar = new JToolBar
+        toolBar.setFloatable(false)
+        val newStreamAction = new Action("New Stream") {
+          toolTip = "Creates a new stream of tweets"
+          var index = 1
+          def apply = {
+            val fs = new FilterSet
+            index += 1
+            val title = "Tweets" + index
+            val provider = new TweetsProvider(username, password, StateRepository.get("highestId", null))
+            val model  = new StatusTableModel(new StatusTableOptions(true), 
+              provider, usersModel, fs, username)
+            val pane = new ToolbarStatusPane(title, model, apiHandlers, fs)
+            tabbedPane.pages += new TabbedPane.Page(title, pane)
+            newFrame.listenTo(model)
+            streams ::= new StreamInfo(title, model, pane)
+          }
+        }
+        toolBar.add(newStreamAction.peer)
+        peer.add(toolBar, BorderLayout.NORTH)
+        add(tabbedPane, BorderPanel.Position.Center)
+      }
 
       reactions += {
         case WindowClosing(_) => {
@@ -69,11 +94,10 @@ object Main extends GUIApplication {
           TagUsers.save
           System.exit(1)
         }
-        case TableContentsChanged(`tweetsModel`, filtered, total) => {
-          tabbedPane.peer.setTitleAt(0, createTweetsTitle(tweetsTitle, filtered, total))
-        }
-        case TableContentsChanged(`repliesModel`, filtered, total) => {
-          tabbedPane.peer.setTitleAt(1, createTweetsTitle(repliesTitle, filtered, total))
+        case TableContentsChanged(model, filtered, total) => {
+          val si = streams.filter(s => s.model == model)(0)
+          tabbedPane.peer.setTitleAt(tabbedPane.peer.indexOfComponent(si.pane.peer), 
+            createTweetsTitle(si.title, filtered, total))
         }
       }
 
@@ -98,7 +122,7 @@ object Main extends GUIApplication {
               
       val paneTitle = "People (" + following.length + ", " + followers.length + ")"
       val pane = new FriendsFollowersPane(apiHandlers, usersModel, following, followers)
-      tabbedPane.pages.append(new TabbedPane.Page(paneTitle, pane))
+      tabbedPane.pages += new TabbedPane.Page(paneTitle, pane)
     })
 
     frame
@@ -115,7 +139,8 @@ object Main extends GUIApplication {
   def main(args: Array[String]): Unit = {
     
     try {
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      UIManager setLookAndFeel UIManager.getSystemLookAndFeelClassName
+      JFrame setDefaultLookAndFeelDecorated true
     } catch {
       case e: Exception => // Ignore
     }
@@ -133,7 +158,7 @@ object Main extends GUIApplication {
   
   def setUpUi {
     init
-    val frame = top
+    val frame = createTopFrame
     frame.pack
     frame.visible = true
   }
@@ -141,3 +166,5 @@ object Main extends GUIApplication {
 }
 
 class ApiHandlers(val sender: Sender, val follower: Follower)
+
+case class StreamInfo(val title: String, val model: StatusTableModel, val pane: StatusPane)
