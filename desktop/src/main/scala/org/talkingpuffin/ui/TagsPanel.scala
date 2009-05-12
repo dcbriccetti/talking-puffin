66044102eb7b3226689dbaba739f2366cf76bb15
Dave Briccetti <daveb@davebsoft.com>
2009-05-11 23:40:39 -0700

    Present the tags as checkboxes with mnemonics starting from A for easy keyboard selection.

--- desktop/src/main/scala/org/talkingpuffin/ui/TagsPanel.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/TagsPanel.scala
@@ -1,29 +1,51 @@
 package org.talkingpuffin.ui
 import _root_.scala.swing.GridBagPanel._
-import _root_.scala.swing.{ListView, Frame, GridBagPanel, UIElement, FlowPanel, BorderPanel, Button, CheckBox, Label, ScrollPane, Action}
+import _root_.scala.swing.{ListView, Frame, GridBagPanel, UIElement, FlowPanel, BorderPanel, BoxPanel, Orientation, Button, CheckBox, Label, ScrollPane, Action}
+
 import filter.TagsRepository
 import java.awt.Dimension
-
+import java.awt.event.KeyEvent
+import javax.swing.{KeyStroke, JCheckBox}
 /**
  * A panel displaying and allowing selection of tags.
  * 
  * @author Dave Briccetti
  */
 class TagsPanel(showTitle: Boolean) extends BorderPanel {
-  val listView = new ListView(TagsRepository.get)
+  val checkBoxView = new CheckBoxView(TagsRepository.get)
   if (showTitle) add(new Label("Tags"), BorderPanel.Position.North)
   
   add(new ScrollPane {
-    contents = listView
+    contents = checkBoxView
   }, BorderPanel.Position.Center)
       
   def selectedTags: List[String] = {
     var selectedTags = List[String]()
-    for (tag <- listView.peer.getSelectedValues) {
+    for (tag <- checkBoxView.getSelectedValues) {
       selectedTags ::= tag.asInstanceOf[String]
     }
     selectedTags        
   }
 }
 
-
+class CheckBoxView(values: List[String]) extends BoxPanel(Orientation.Vertical) {
+  var i = 0
+  for (value <- values) {
+    val keyVal = KeyEvent.VK_A + i
+      contents += new CheckBox("" + keyVal.toChar + ": " + value) {
+      peer.setMnemonic(keyVal)
+      i += 1
+    }
+  }
+  
+  def getSelectedValues: Seq[String] = {
+    var values = List[String]()
+    for (child <- peer.getComponents) {
+      child match {
+        case cb: JCheckBox => if (cb.isSelected) values = cb.getText :: values
+        case _ =>
+      }
+    }
+    values
+  }
+}
