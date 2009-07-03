6e26b9c04f391a66a0605e06f74157c23691e427
mccv <mark.mcbride@gmail.com>
2009-07-03 10:09:13 -0700

    adding command-w facilities for mac, making sure menubar gets attached to undocked frames

--- desktop/src/main/scala/org/talkingpuffin/ui/TopFrames.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/TopFrames.scala
@@ -2,7 +2,7 @@ package org.talkingpuffin.ui
 
 import apache.log4j.Logger
 import mac.QuitHandler
-
+import java.awt.Frame
 /**
  * Keeps track of top-level frames.
  */
@@ -12,6 +12,20 @@ object TopFrames {
 
   QuitHandler register TopFrames.closeAll
 
+  def closeCurrentWindow(){
+    frames.find(_.peer.isFocused) match{
+      case Some(frame) => close(frame)
+      case _ => closeOtherFrame()
+    }
+  }
+
+  def closeOtherFrame(){
+    val frames = Frame.getFrames()
+    frames.find(_.isFocused) match {
+      case Some(frame) => frame.dispose
+      case _ => // noop
+    }
+  }
   def addFrame(f: TopFrame){
     frames = f :: frames
     log debug "Frame added. Number of frames is " + frames.size + "."
@@ -33,13 +47,16 @@ object TopFrames {
   
   def numFrames = frames.size
 
+  def close(frame:TopFrame): Unit = {
+      frame.dispose
+      frame.saveState
+      TopFrames removeFrame frame
+  }
   def closeAll: Unit = closeAll(frames)
 
   def closeAll(frames: List[TopFrame]): Unit = frames match {
     case frame :: rest => {
-      frame.dispose
-      frame.saveState
-      TopFrames removeFrame frame
+      close(frame)
       closeAll(rest)
     }
     case Nil =>
