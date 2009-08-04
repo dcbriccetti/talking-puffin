85a0022a7a69a104883d067769c6591992847328
Dave Briccetti <daveb@davebsoft.com>
2009-08-04 00:46:36 -0700

    Modify ljantzen’s tpuf-53 enhancement to not reset all the option menu item defaults to true.

--- desktop/src/main/scala/org/talkingpuffin/ui/MainMenuBar.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/MainMenuBar.scala
@@ -13,7 +13,6 @@ import state.{GlobalPrefs, PrefKeys}
  */
 class MainMenuBar extends MenuBar {
   val prefs = GlobalPrefs.prefs
-  private val log = Logger.getLogger("MainMenuBar " + hashCode)
   
   contents += new Menu("Session") {
     contents += new MenuItem(new Action("New...") {
@@ -24,8 +23,11 @@ class MainMenuBar extends MenuBar {
   }
   contents += new Menu("Options") {
     object ItemFactory {
-      def apply(title: String, tooltip: String, prefKey: String): MenuItem = {
-        val item = new CheckMenuItem(title) {this.tooltip = tooltip; selected = prefs.getBoolean(prefKey, true)}
+      def apply(title: String, tooltip: String, prefKey: String, onByDefault: Boolean): MenuItem = {
+        val item = new CheckMenuItem(title) {
+          this.tooltip = tooltip; 
+          selected = prefs.getBoolean(prefKey, onByDefault)
+        }
         listenTo(item)
         reactions += {
           case r: ButtonClicked => if (r.source == item) {
@@ -36,12 +38,13 @@ class MainMenuBar extends MenuBar {
         item
       }
     }
-    contents += ItemFactory("Use animations", "Enables simple, useful animations", PrefKeys.USE_ANIMATIONS)
-    contents += ItemFactory("Use real names", "Display real names", PrefKeys.USE_REAL_NAMES)
+    contents += ItemFactory("Use animations", "Enables simple, useful animations", 
+      PrefKeys.USE_ANIMATIONS, false)
+    contents += ItemFactory("Use real names", "Display real names", PrefKeys.USE_REAL_NAMES, true)
     contents += ItemFactory("Look up locations", "Enables lookup of locations from latitude and longitude",
-      PrefKeys.LOOK_UP_LOCATIONS)
+      PrefKeys.LOOK_UP_LOCATIONS, false)
     contents += ItemFactory("Expand URLs", "Enables fetching original URL from shortened form", 
-      PrefKeys.EXPAND_URLS)
+      PrefKeys.EXPAND_URLS, false)
   }
   
 }
