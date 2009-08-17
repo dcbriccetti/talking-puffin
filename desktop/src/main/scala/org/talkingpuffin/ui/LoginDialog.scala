b8c6fd46a50100f26b116d5d2f1730f798ceee15
Dave Briccetti <daveb@davebsoft.com>
2009-08-17 16:34:39 -0700

    tpuf-54:  Remember more than one user/password in login dialog

--- desktop/src/main/scala/org/talkingpuffin/ui/LoginDialog.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/LoginDialog.scala
@@ -1,98 +1,114 @@
 package org.talkingpuffin.ui
 
-import _root_.scala.swing._
-import _root_.scala.swing.event.{ButtonClicked, EditDone}
+import _root_.scala.swing.{ComboBox, CheckBox, Label, Button, Frame, PasswordField, Publisher, 
+FlowPanel, GridBagPanel, TextField}
+import ComboBox._
 import _root_.scala.xml.Node
+import collection.mutable.Subscriber
 import java.awt.event.{ActionEvent, ActionListener}
 import java.awt.Color
+import java.util.prefs.Preferences
 import javax.swing.JDialog
+import javax.swing.SpringLayout.Constraints
 import org.talkingpuffin.twitter.{TwitterSession,AuthenticatedSession}
 import state.{GlobalPrefs, PreferencesFactory}
 import LongRunningSpinner._
-import event.Event
+import swing.event.{SelectionChanged, ButtonClicked, EditDone, Event}
+import talkingpuffin.util.Loggable
+import util.Cancelable
 
 /**
  * Collect user name and password for Twitter authentication.
  */
-
 class LoginDialog(cancelPressed: => Unit, 
     startup: (String, String, AuthenticatedSession) => Unit)
-    extends Frame {
+    extends Frame with Cancelable with Loggable {
   
   title = "TalkingPuffin - Log In"
   def username = usernameTextField.text
   def password = new String(passwordTextField.password)
   private var ok = false
 
-  val usernameKey = "username"
-  val pwdKey = "password"
-  val prefs = GlobalPrefs.prefs
-  private var storedUser = prefs.get(usernameKey, null)
-  private var storedPwd = prefs.get(pwdKey, null)
-  
-  private val loginButton = new Button("Log In")
+  private val logInButton = new Button("Log In")
+  private val logInAllButton = new Button("Log In All")
   private val cancelButton = new Button("Cancel")
   private val saveUserInfoCheckBox = new CheckBox("Remember me (saves password unencrypted)")
   private val infoLabel = new Label(" ")
+  private val up = new UsersPasswords()
 
-  private val usernameTextField = new TextField(storedUser) {columns=20}
-  private val passwordTextField = new PasswordField(storedPwd) {columns=20}
+  private val comboBox = if (up.users.length > 0) new ComboBox(up.users) else null
+  private val usernameTextField = new TextField() {columns=20}
+  private val passwordTextField = new PasswordField() {columns=20}
   
-  private val enterReaction: PartialFunction[Event, Unit] = { case EditDone(f) => loginButton.peer.doClick() }
+  private val enterReaction: PartialFunction[Event, Unit] = { case EditDone(f) => logInButton.peer.doClick() }
   
   setUpEnterClick(true)
   
   def storeUserInfoIfSet() {
-    if(saveUserInfoCheckBox.peer.isSelected) {
-      prefs.put(usernameKey, username)
-      prefs.put(pwdKey, password)
-    }
-    else  {
-      prefs.remove(usernameKey)
-      prefs.remove(pwdKey)
-    }
+    if(saveUserInfoCheckBox.peer.isSelected) up.save(username, password)
+    else up.remove(username)
+    up.save()
   }
 
-  if (storedUser != null) saveUserInfoCheckBox.peer.setSelected(true)
+  saveUserInfoCheckBox.peer.setSelected(true)
   
   private def setUpEnterClick(enable: Boolean) {
+    if (comboBox != null) enterDoesLoginClick(comboBox, enable)
     enterDoesLoginClick(usernameTextField, enable)
     enterDoesLoginClick(passwordTextField, enable)
   }
   
-  private def enterDoesLoginClick(t: TextField, enable: Boolean) {
+  private def enterDoesLoginClick(t: Publisher, enable: Boolean) {
     if (enable) t.reactions += enterReaction
     else t.reactions -= enterReaction
   }
   
   contents = new GridBagPanel {
-    border = Swing.EmptyBorder(5, 5, 5, 5)
-    add(new Label("User name"), new Constraints {grid=(0,0)})
-    add(usernameTextField,      new Constraints {grid=(1,0)})
-    add(new Label("Password"),  new Constraints {grid=(0,1)})
-    add(passwordTextField,      new Constraints {grid=(1,1)})
-    add(infoLabel,              new Constraints {grid=(0,2); gridwidth=2; anchor=GridBagPanel.Anchor.West})
+    border = scala.swing.Swing.EmptyBorder(5, 5, 5, 5)
+    add(if (comboBox != null ) comboBox else usernameTextField, 
+      new Constraints {grid = (0, 0); gridwidth=2; anchor=GridBagPanel.Anchor.West})
+    add(new Label("User name"), new Constraints {grid=(0,1)})
+    add(usernameTextField, new Constraints {grid = (1, 1); anchor=GridBagPanel.Anchor.West})
+    add(new Label("Password"),  new Constraints {grid=(0,2); anchor=GridBagPanel.Anchor.West})
+    add(passwordTextField,      new Constraints {grid=(1,2); anchor=GridBagPanel.Anchor.West})
+    add(infoLabel,              new Constraints {grid=(0,4); gridwidth=2; anchor=GridBagPanel.Anchor.West})
     
     add(new FlowPanel {
-      contents += loginButton
+      contents += logInButton
+      // TODO  if (up.users.length > 1) contents += logInAllButton
       contents += cancelButton
       contents += saveUserInfoCheckBox
     }, new Constraints {grid=(0,3); gridwidth=2})
     
     reactions += {
-      case ButtonClicked(b) =>
-        ok = b == loginButton
-        if (ok) {
-          handleLogin
-        } else {
-          // Cancel pressed
-          LoginDialog.this.visible = false
-          cancelPressed
-        }
+      case ButtonClicked(`logInButton`) => {storeUserInfoIfSet(); handleLogin}
+      case ButtonClicked(`logInAllButton`) => {storeUserInfoIfSet(); handleAllLogins}
+      case ButtonClicked(`cancelButton`) =>
+        LoginDialog.this.visible = false
+        cancelPressed
+      case SelectionChanged(`comboBox`) => showSelectedUser
     }
-    listenTo(loginButton)
+    if (comboBox != null) listenTo(comboBox.selection)
+    listenTo(logInButton)
+    listenTo(logInAllButton)
     listenTo(cancelButton)
   }
+
+  private def showSelectedUser {
+    if (comboBox != null) {
+      val item = comboBox.selection.item
+      usernameTextField.text = item
+      up.passwordFor(item) match {
+        case Some(pw) =>
+          passwordTextField.peer.setText(pw)
+        case _ =>
+      }
+    }
+  }
+  
+  showSelectedUser
+
+  override def notifyOfCancel = cancelPressed
   
   private def handleLogin {
     enableButtons(false)
@@ -103,7 +119,6 @@ class LoginDialog(cancelPressed: => Unit,
         val sess = TwitterSession(username,password)
         if(sess.verifyCredentials){
             loggedInUser = sess
-            storeUserInfoIfSet()
             true
         }else{
             infoLabel.foreground = Color.RED
@@ -123,11 +138,13 @@ class LoginDialog(cancelPressed: => Unit,
     )
   }
   
-  defaultButton = loginButton
+  private def handleAllLogins = { /* TODO */ }
+  
+  defaultButton = logInButton
   
   private def enableButtons(enable: Boolean) {
     cancelButton.enabled = enable
-    loginButton.enabled = enable
+    logInButton.enabled = enable
     saveUserInfoCheckBox.enabled = enable
     setUpEnterClick(enable)
   }
@@ -137,4 +154,43 @@ class LoginDialog(cancelPressed: => Unit,
     peer.setLocationRelativeTo(null)
     visible = true
   }
+}
+
+object UserPassword {
+  val SEP = '&'
+}
+
+case class UserPassword(var user: String, var password: String) {
+  def serialized = user + UserPassword.SEP + password
+}
+
+class UsersPasswords extends Loggable {
+  val usersKey = "users"
+  private val prefs = GlobalPrefs.prefs
+  
+  var data: List[UserPassword] = load
+  
+  def save() {
+    val ser = data.map(_.serialized).mkString("\t")
+    info("Saving " + ser)
+    prefs.put(usersKey, ser)
+  }
+  def save(username: String, password: String) {
+    data = UserPassword(username, password) :: data.filter(_.user != username)
+  }
+  def remove(username: String) = data = data.filter(_.user != username)
+  
+  def load: List[UserPassword] = 
+    for (u <- List.fromString(prefs.get(usersKey, ""), '\t'); uspw = List.fromString(u, UserPassword.SEP)) 
+      yield UserPassword(uspw(0), uspw(1))
+  
+  def users: List[String] = data.map(_.user)
+  
+  def passwordFor(username: String): Option[String] = 
+    data.find(_.user == username) match {
+      case Some(userPassword) =>
+        info("Found " + userPassword.password)
+        Some(userPassword.password)
+      case _ => None
+    }
 }
--- desktop/src/main/scala/org/talkingpuffin/ui/util/Cancelable.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/util/Cancelable.scala
@@ -1,18 +1,30 @@
 package org.talkingpuffin.ui.util
 
 import java.awt.event.KeyEvent
-import javax.swing.{JDialog, JComponent, KeyStroke}
-import swing.Action
-
+import javax.swing.{JRootPane, JDialog, JComponent, KeyStroke}
+import swing.{Frame, Action}
 /**
  * Make a JDialog cancelable with the Escape key
  */
 trait Cancelable {
-  val dlg = this.asInstanceOf[JDialog] 
-  val cancelAction = Action("Cancel") {dlg.setVisible(false)}
-  val rp = dlg.getRootPane
+  var cancelAction: Action = null
+  var rp: JRootPane = null
+  
+  if (isInstanceOf[JDialog]) {
+    val dlg = this.asInstanceOf[JDialog] 
+    cancelAction = Action("Cancel") {dlg.setVisible(false); notifyOfCancel}
+    rp = dlg.getRootPane
+  }
+  if (isInstanceOf[Frame]) {
+    val frame = this.asInstanceOf[Frame] 
+    cancelAction = Action("Cancel") {frame.visible = false; notifyOfCancel}
+    rp = frame.peer.getRootPane
+  }
+
   rp.getActionMap.put(cancelAction.title, cancelAction.peer)
   rp.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
     KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), cancelAction.title)
+  
+  def notifyOfCancel {}
 }
  
