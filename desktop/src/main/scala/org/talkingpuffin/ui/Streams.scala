07132e2aa2b17f637fee93ea1e3608fced0567c6
Dave Briccetti <daveb@davebsoft.com>
2009-08-30 23:47:45 -0700

    Simplify

--- desktop/src/main/scala/org/talkingpuffin/ui/Streams.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/Streams.scala
@@ -65,10 +65,7 @@ class Streams(val service: String, val user: AuthenticatedSession, session: Sess
   }
 
   private def createTweetsTitle(paneTitle: String, filtered: Int, total: Int): String = 
-    paneTitle + " (" + (total - filtered match {
-      case 0 => total
-      case _ => filtered + "/" + total
-    }) + ")"
+    paneTitle + " (" + (if (total == filtered) total else filtered + "/" + total) + ")"
 
   private def createView(source: TweetsProvider, title: String, include: Option[String]): View = {
     val fs = new FilterSet(session)
