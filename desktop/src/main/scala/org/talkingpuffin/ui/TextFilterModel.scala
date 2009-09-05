3568df2140c4ceeab5f3e3fc7a3b4436303b206f
Dave Briccetti <daveb@davebsoft.com>
2009-09-04 23:43:50 -0700

    Simplify

--- desktop/src/main/scala/org/talkingpuffin/ui/TextFilterModel.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/TextFilterModel.scala
@@ -1,25 +1,18 @@
 package org.talkingpuffin.ui
 
-import filter.{TextFilters, TextFilter}
+import filter.TextFilters
 import javax.swing.table.AbstractTableModel
 
 /**
  * Model for table of TextFilters 
  */
-
 class TextFilterModel(textFilters: TextFilters) extends AbstractTableModel {
-  val colNames = List("Text", "Regex")
+  private val colNames = List("Text", "Regex")
+  
   override def getColumnName(column: Int) = colNames(column)
-
   def getColumnCount = 2
-
   def getRowCount = textFilters.list.length
-
-  override def getColumnClass(columnIndex: Int) = {
-    columnIndex match {
-      case _ => classOf[String]
-    }
-  }
+  override def getColumnClass(columnIndex: Int) = classOf[String] 
 
   override def getValueAt(rowIndex: Int, columnIndex: Int): Object = {
     val row = textFilters.list(rowIndex)
