21b18f1de574ec7394a98a89a148f29ce7bd4247
Dave Briccetti <daveb@davebsoft.com>
2009-09-24 00:49:01 -0700

    Make real tests out of my asserts. Thanks, @ljantzen. Remove one unused test file.

--- desktop/src/test/scala/org/talkingpuffin/filter/RetweetsTest.scala
+++ desktop/src/test/scala/org/talkingpuffin/filter/RetweetsTest.scala
@@ -1,9 +1,15 @@
 import org.talkingpuffin.filter.Retweets
-
-object RetweetsTest {
-  def main(args: Array[String]): Unit = {
+import org.specs.runner.JUnit4
+import org.specs.Specification
+ 
+class RetweetsTest extends JUnit4(RetweetsSpec)
+ 
+object RetweetsSpec extends Specification {
+    
+  "Retweets are identified correctly" in {
     assert(Retweets.fromFriend_?("RT @dave Hi", List("dave", "mary")))
     assert(Retweets.fromFriend_?("Hi (via @dave)", List("dave", "mary")))
-    assert(! Retweets.fromFriend_?("Hi (via @dave2)", List("dave", "mary")))
+    assert(!Retweets.fromFriend_?("Hi (via @dave2)", List("dave", "mary")))
   }
 }
+ 
