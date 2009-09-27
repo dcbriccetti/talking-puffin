a1b43ba646927096be9f77d36d7b29880fb6bd24
Dave Briccetti <daveb@davebsoft.com>
2009-09-27 11:48:06 -0700

    Simplify, fix bug with accelerators, remove blocked user from Relationships

--- desktop/src/main/scala/org/talkingpuffin/ui/UserActions.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/UserActions.scala
@@ -9,17 +9,28 @@ import twitter.AuthenticatedSession
  * Handles user actions like follow
  */
 class UserActions(twitterSession: AuthenticatedSession, rels: Relationships) {
-  def follow(names: List[String]) = process(names, 
-      twitterSession.createFriendship, showFollowErr(_,"following",_))
-  def unfollow(names: List[String]) = {
-    process(names, twitterSession.destroyFriendship, showFollowErr(_, "unfollowing", _))
+  def follow(names: List[String]) = process(names, twitterSession.createFriendship, "following")
+  
+  def unfollow(names: List[String]) {
+    process(names, twitterSession.destroyFriendship, "unfollowing")
     rels.removeFriendsWithScreenNames(names)
   }
-  def block  (names: List[String]) = process(names, twitterSession.blockUser  , showFollowErr(_,"block",_))
-  def unblock(names: List[String]) = process(names, twitterSession.unblockUser, showFollowErr(_,"block",_))
+
+  def block(names: List[String]) {
+    process(names, twitterSession.blockUser, "block")
+    rels.removeFriendsWithScreenNames(names)
+  }
   
-  private def process(names:List[String], action:((String) => Unit), errHandler:((Throwable,String) => Unit)) = 
-    names foreach {name => try {action(name)} catch {case e: Throwable => errHandler(e,name)}}
+  def unblock(names: List[String]) = process(names, twitterSession.unblockUser, "unblock")
+  
+  private def process(names:List[String], action:((String) => Unit), actionName: String) = 
+    names foreach {name => 
+      try {
+        action(name)
+      } catch {
+        case e: Throwable => showFollowErr(e, actionName, name)
+      }
+    }
 
   private def showFollowErr(e:Throwable,action:String,screenName:String){
     JOptionPane.showMessageDialog(null, "Error " + action + " " + screenName)
@@ -27,11 +38,11 @@ class UserActions(twitterSession: AuthenticatedSession, rels: Relationships) {
 }
 
 object UserActions {
+  private val Shift = java.awt.event.InputEvent.SHIFT_DOWN_MASK
+  private val shortcutKeyMask = Toolkit.getDefaultToolkit.getMenuShortcutKeyMask
+
   val FollowAccel   = KeyStroke.getKeyStroke(KeyEvent.VK_F, shortcutKeyMask)
   val UnfollowAccel = KeyStroke.getKeyStroke(KeyEvent.VK_F, shortcutKeyMask | Shift)
   val BlockAccel    = KeyStroke.getKeyStroke(KeyEvent.VK_B, shortcutKeyMask)  
   val UnblockAccel  = KeyStroke.getKeyStroke(KeyEvent.VK_B, shortcutKeyMask | Shift)  
-
-  private val Shift = java.awt.event.InputEvent.SHIFT_DOWN_MASK
-  private val shortcutKeyMask = Toolkit.getDefaultToolkit.getMenuShortcutKeyMask
 }
