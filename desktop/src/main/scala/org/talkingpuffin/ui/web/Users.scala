7aacf8b342a886c4eba71fbbf67776e954adfc7d
Dave Briccetti <daveb@davebsoft.com>
2009-10-08 20:50:28 -0700

    Get Web app working again

--- desktop/src/main/scala/org/talkingpuffin/ui/web/Users.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/web/Users.scala
@@ -6,38 +6,26 @@ import org.talkingpuffin.ui.{UserSelection, Relationships, UsersModel}
 
 class UserRow(val picUrl: String, val arrows: String, val screenName: String, val name: String,
     val numFriends: Int, val numFollowers: Int,
-    val location: String, val description: String, val status: String) extends Serializable {
-  def getPicUrl = picUrl
-  def getArrows = arrows
-  def getScreenName = screenName
-  def getName = name
-  def getNumFriends = numFriends
-  def getNumFollowers = numFollowers
-  def getLocation = location
-  def getDescription = description
-  def getStatus = status
-}
+    val location: String, val description: String, val status: String) extends Serializable
 
 /**
  * Users managed bean.
  */
 class Users extends Serializable {
   var session: AuthenticatedSession = _
-  def setSession(session: AuthenticatedSession) = this.session = session
-  var following:List[TwitterUser] = _
-  var followers:List[TwitterUser] = _
+  val rels = new Relationships
 
-  def getArrows(user:TwitterUser) = {
-    val friend = following.contains(user)
-    val follower = followers.contains(user)
+  def setSession(session: AuthenticatedSession) = this.session = session
+
+  def getArrows(user:TwitterUser): String = {
+    val friend = rels.friends.contains(user)
+    val follower = rels.followers.contains(user)
     if (friend && follower) "↔" else if (friend) "→" else "←"
   }
 
   def getUsers: Array[TwitterUser] = {
-    val rels = new Relationships() {
-      friends   = session.getFriends  (TwitterArgs.maxResults(200))
-      followers = session.getFollowers(TwitterArgs.maxResults(200))
-    }
+    rels.friends   = session.getFriends  (TwitterArgs.maxResults(200))
+    rels.followers = session.getFollowers(TwitterArgs.maxResults(200))
     UsersModel(rels, UserSelection(true, true, None)).users.toArray
   }
 }
