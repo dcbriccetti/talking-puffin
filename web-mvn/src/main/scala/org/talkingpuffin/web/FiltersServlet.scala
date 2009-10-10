320bff414f673e26c9d223fb398f7da48d836b52
Dave Briccetti <daveb@davebsoft.com>
2009-10-10 11:26:38 -0700

    New filters

--- web-mvn/src/main/scala/org/talkingpuffin/web/FiltersServlet.scala
+++ web-mvn/src/main/scala/org/talkingpuffin/web/FiltersServlet.scala
@@ -12,9 +12,11 @@ class FiltersServlet extends HttpServlet {
 just joined a video chat at http\://tinychat\.com.*
 I just ousted .* as the mayor of .* on @foursquare!.*
 ran .* on .* at .* with a pace of .*
+Just completed a .* run with @runkeeper, check it out.*
 just voted .* on .* vote too .*
 Just added myself to the .*wefollow.*
 .*add a \#twibbon to your avatar now.*
+Hey, I just added you to my Mafia family. You should accept my invitation! :) Click here: .*
 """)
   }
 }
