00e54194bde8df3ed72d5c646add2bf0960cb08b
Dave Briccetti <daveb@davebsoft.com>
2009-10-17 14:23:08 -0700

    Move some code around, improve titles with docking

--- desktop/src/main/scala/org/talkingpuffin/ui/util/Dockable.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/util/Dockable.scala
@@ -1,9 +1,9 @@
 package org.talkingpuffin.ui.util
 
-import swing.{Action}
-import javax.swing.JToggleButton
-import _root_.scala.swing.{Frame, TabbedPane, Component}
-import javax.swing.{JTabbedPane, JFrame, SwingUtilities}
+import swing.TabbedPane.Page
+import java.awt.Dimension
+import javax.swing.{JFrame, SwingUtilities, JComponent, JToggleButton}
+import swing.{Action, Frame, TabbedPane, Component}
 import org.talkingpuffin.Session
 import org.talkingpuffin.ui.{MainMenuBar}
 import org.talkingpuffin.util.Loggable
@@ -16,6 +16,10 @@ trait Dockable extends Component with Loggable {
   val longTitle: String
   val shortTitle: String
   
+  private var titleSuffix_ = ""
+  def titleSuffix = titleSuffix_
+  private val tabbedPane = session.windows.tabbedPane
+  
   var dockedButton: JToggleButton = _ 
   val dockedAction = new Action("Docked") {
     toolTip = "Docks or frees the pane"
@@ -25,21 +29,35 @@ trait Dockable extends Component with Loggable {
   dockedButton.setSelected(true)
 
   private def undock {
+    peer.asInstanceOf[JComponent].setPreferredSize(new Dimension(800,700))
     new Frame {
-      title = longTitle + " " + (peer.getParent match {
-        case tp: JTabbedPane => tp.getTitleAt(tp.indexOfComponent(peer))
-        case _ => ""
-      })
+      title = withSuffix(longTitle)
       contents = Dockable.this
       menuBar = new MainMenuBar(session.windows.streams.providers)
+      peer.setLocationRelativeTo(null)
     }.visible = true
   }
   
   private def dock {                                            
     val frame = SwingUtilities.getAncestorOfClass(classOf[JFrame], peer).asInstanceOf[JFrame]
-    session.windows.tabbedPane.pages += new TabbedPane.Page(shortTitle, this) {tip = longTitle}
+    val newPage = new Page(withSuffix(shortTitle), this) {tip = longTitle}
+    tabbedPane.pages += newPage
+    tabbedPane.selection.page = newPage 
     frame.dispose
   }
 
+  def titleSuffix_=(newTitleSuffix: String) = {
+    titleSuffix_ = newTitleSuffix
+    tabbedPane.peer.indexOfComponent(peer) match {
+      case -1 => 
+        SwingUtilities.getAncestorOfClass(classOf[JFrame], peer) match {
+          case null =>
+          case jf => jf.asInstanceOf[JFrame].setTitle(withSuffix(longTitle)) 
+        }
+      case i => tabbedPane.peer.setTitleAt(i, withSuffix(shortTitle))
+    }
+  }
+  
+  private def withSuffix(title: String) = if (titleSuffix_.length == 0) title else title + " " + titleSuffix_
 }
   
