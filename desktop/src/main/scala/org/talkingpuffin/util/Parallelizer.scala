eeecd92bb596d73f94e931dbc79db032a1a86f97
Dave Briccetti <daveb@davebsoft.com>
2009-10-19 20:45:09 -0700

    Shut down the pool after using it

--- desktop/src/main/scala/org/talkingpuffin/util/Parallelizer.scala
+++ desktop/src/main/scala/org/talkingpuffin/util/Parallelizer.scala
@@ -3,11 +3,16 @@ package org.talkingpuffin.util
 import java.util.concurrent.{Callable, Executors}
 
 object Parallelizer {
-  def run[T,A,F](numThreads: Int, args: List[A], f: (A) => T) = {
+  /**
+   * Runs, in the number of threads requested, the function f, giving it each A of args, returning a List[T]
+   */
+  def run[T,A,F](numThreads: Int, args: List[A], f: (A) => T): List[T] = {
     val pool = Executors.newFixedThreadPool(numThreads)
-    (for {
+    val result: List[T] = (for {
       arg <- args
       future = pool.submit(new Callable[T] {def call = f(arg)})
     } yield future).map(_.get)
+    pool.shutdown
+    result
   }
 }
