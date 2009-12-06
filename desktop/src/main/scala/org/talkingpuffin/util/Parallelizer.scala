66e8b6a520ac6e6366231052f520eaff7a8f3642
Dave Briccetti <daveb@davebsoft.com>
2009-12-06 14:28:38 -0800

    Accept a Seq.

--- desktop/src/main/scala/org/talkingpuffin/util/Parallelizer.scala
+++ desktop/src/main/scala/org/talkingpuffin/util/Parallelizer.scala
@@ -6,12 +6,12 @@ object Parallelizer {
   /**
    * Runs, in the number of threads requested, the function f, giving it each A of args, returning a List[T]
    */
-  def run[T,A](numThreads: Int, args: List[A], f: (A) => T): List[T] = {
+  def run[T,A](numThreads: Int, args: Seq[A], f: (A) => T): List[T] = {
     val pool = Executors.newFixedThreadPool(numThreads)
     val completionService = new ExecutorCompletionService[T](pool)
     args.foreach(arg => completionService.submit(new Callable[T] {def call = f(arg)}))
     val result = args map(_ => completionService.take.get)
     pool.shutdown
-    result
+    result.toList
   }
 }
