9e8f31c57da48b1921c457ccb7aae1362f0396ff
Dave Briccetti <daveb@davebsoft.com>
2009-12-21 18:19:45 -0800

    Streamline

--- desktop/src/main/scala/org/talkingpuffin/ui/ActionProcessor.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/ActionProcessor.scala
@@ -6,16 +6,15 @@ trait ActionProcessor {
   val session: Session
   
   def process[T <: Object](items: Seq[T], action: ((T) => Unit), actionName: String, msg: String) =
-    items.foreach(item =>   
-      try {
-        action(item)
-        session.addMessage(String.format(msg, item))
-      } catch {
-        case e: Throwable => showActionErr(e, actionName, item)
-      }
+    items.foreach(item =>
+      session.addMessage( 
+        try {
+          action(item)
+          String.format(msg, item)
+        } catch {
+          case e: Throwable => "Error " + actionName + " " + item.toString
+        }
+      )
     )
-
-  private def showActionErr[T](e: Throwable, actionName: String, item: T) =
-    session.addMessage("Error " + actionName + " " + item.toString)
   
 }
