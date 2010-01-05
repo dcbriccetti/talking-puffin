8fd8803f8cdbbbc1353d436f58cc3adaf8f6c98c
Dave Briccetti <daveb@davebsoft.com>
2010-01-05 00:19:23 -0800

    Fix from Leif and formatting corrections.

--- desktop/src/main/scala/org/talkingpuffin/time/TimeUtil.scala
+++ desktop/src/main/scala/org/talkingpuffin/time/TimeUtil.scala
@@ -9,7 +9,7 @@ import org.talkingpuffin.ui.HtmlFormatter
 object TimeUtil{
   val fmt = DateTimeFormat.forPattern("MM/dd HH:mm:ss")
   val fmtNoDay = DateTimeFormat.forPattern("HH:mm:ss")
-  def formatAge(date: Date) : String  = formatAge(new DateTime(date))
+  def formatAge(date: Date): String  = formatAge(new DateTime(date))
   def formatAge(date: DateTime): String = {
     if (showAsAge_?)
       new TimeFormatter(toAge(date)).colonSeparated
@@ -17,15 +17,14 @@ object TimeUtil{
       if (isToday(date)) fmtNoDay.print(date) else fmt.print(date)
   }
   def showAsAge_? = GlobalPrefs.isOn(PrefKeys.SHOW_TWEET_DATE_AS_AGE)
-  def isToday(d1: DateTime):Boolean = {
+  def isToday(d1: DateTime): Boolean = {
      val now = new DateTime() 
-     d1.getDayOfYear == now.getDayOfYear &&  d1.getYear == now.getYear 
+     d1.getDayOfYear == now.getDayOfYear && d1.getYear == now.getYear 
   }
-  def toAge(date: DateTime) : Long=  Seconds.secondsBetween(date,now).getSeconds
+  def toAge(date: DateTime): Long = Seconds.secondsBetween(date,now).getSeconds
   def now = new DateTime
-
-  def asHTML(value : Any) : String = asHTML(value.asInstanceOf[Date])
-  def asHTML(time :  Date) :String = {
+  def asHTML(value: Any): String = asHTML(value.asInstanceOf[Date])
+  def asHTML(time: Date): String = {
    HtmlFormatter.htmlAround("<font size='-1' face='helvetica'>" +
        formatAge(time) + "</font>")
    }
