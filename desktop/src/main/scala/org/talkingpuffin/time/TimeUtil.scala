ec8b491008202895226b596b2a0d73b0c4d471fe
Leif Jantzen <ljantzen@gmail.com>
2010-01-05 09:03:00 +0100

    fixed TimeUtil.isToday

--- desktop/src/main/scala/org/talkingpuffin/time/TimeUtil.scala
+++ desktop/src/main/scala/org/talkingpuffin/time/TimeUtil.scala
@@ -17,7 +17,10 @@ object TimeUtil{
       if (isToday(date)) fmtNoDay.print(date) else fmt.print(date)
   }
   def showAsAge_? = GlobalPrefs.isOn(PrefKeys.SHOW_TWEET_DATE_AS_AGE)
-  def isToday(d1: DateTime):Boolean = d1.getDayOfYear == new DateTime().getDayOfYear
+  def isToday(d1: DateTime):Boolean = {
+     val now = new DateTime() 
+     d1.getDayOfYear == now.getDayOfYear &&  d1.getYear == now.getYear 
+  }
   def toAge(date: DateTime) : Long=  Seconds.secondsBetween(date,now).getSeconds
   def now = new DateTime
 
