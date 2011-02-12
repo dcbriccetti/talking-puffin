8c2859f9201ec64a51a166fe1b03f0a145d5945c
Dave Briccetti <daveb@davebsoft.com>
2011-02-11 22:59:24 -0800

    Remove some redundancy in creating tweet providers

--- desktop/src/main/scala/org/talkingpuffin/ui/DataProviders.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/DataProviders.scala
@@ -3,24 +3,19 @@ package org.talkingpuffin.ui
 import java.util.prefs.Preferences
 import org.talkingpuffin.state.PrefKeys._
 import org.talkingpuffin.Session
+import twitter4j.{Status, ResponseList, Paging}
 
 class DataProviders(val session: Session, prefs: Preferences, progress: LongOpListener) {
-  val prefKeys = List(HIGHEST_ID, HIGHEST_MENTION_ID, HIGHEST_RETWEET_OF_ME_ID, 
+  private val prefKeys = List(HIGHEST_ID, HIGHEST_MENTION_ID, HIGHEST_RETWEET_OF_ME_ID,
     HIGHEST_RETWEET_BY_ME_ID, HIGHEST_RETWEET_TO_ME_ID, 
     HIGHEST_RECEIVED_DM_ID, HIGHEST_SENT_DM_ID)
 
-  private def getHighest(idx: Int): Option[Long] = 
-    prefs.get(prefKeys(idx), null) match {case null => None; case v => Some(v.toLong)} 
-
   private val tw = session.twitter
-  val following     = new CommonTweetsProvider("Following", session, getHighest(0), progress, tw.getHomeTimeline)
-  val mentions      = new CommonTweetsProvider("Mentions",  session, getHighest(1), progress, tw.getMentions,
-    Some(StatusTableModelCust.Mentions))
-  val retweetsOfMe  = new CommonTweetsProvider("RTs of Me", session, getHighest(2), progress, tw.getRetweetsOfMe)
-  val retweetedByMe = new CommonTweetsProvider("RTs by Me", session, getHighest(3), progress, tw.getRetweetedByMe)
-  val retweetedToMe = new CommonTweetsProvider("RTs to Me", session, getHighest(4), progress, tw.getRetweetedToMe)
-  /*todo val dmsReceived   = new DmsReceivedProvider  (session, getHighest(5), progress)
-  val dmsSent       = new DmsSentProvider      (session, getHighest(6), progress)*/
+  val following     = create("Following", 0, tw.getHomeTimeline)
+  val mentions      = create("Mentions",  1, tw.getMentions, Some(StatusTableModelCust.Mentions))
+  val retweetsOfMe  = create("RTs of Me", 2, tw.getRetweetsOfMe)
+  val retweetedByMe = create("RTs by Me", 3, tw.getRetweetedByMe)
+  val retweetedToMe = create("RTs to Me", 4, tw.getRetweetedToMe)
 
   val providers = List(following, mentions, retweetsOfMe, retweetedByMe,
     retweetedToMe)//todo, dmsReceived, dmsSent)
@@ -28,4 +23,11 @@ class DataProviders(val session: Session, prefs: Preferences, progress: LongOpLi
   val providersAndPrefKeys = providers zip prefKeys
   
   def stop = providers.foreach(_.stop)
+
+  private def getHighest(idx: Int): Option[Long] =
+    prefs.get(prefKeys(idx), null) match {case null => None; case v => Some(v.toLong)}
+
+  private def create(title: String, idx: Int, twFunc: (Paging) => ResponseList[Status],
+    statusTableModelCust: Option[StatusTableModelCust.Value] = None) =
+    new CommonTweetsProvider(title, session, getHighest(idx), progress, twFunc, statusTableModelCust)
 }
