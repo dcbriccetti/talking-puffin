03eba2ba54eb65ae2c31f0b98296d14cfb7f3689
Dave Briccetti <daveb@davebsoft.com>
2011-02-15 03:19:39 -0800

    Increase from 3 to 5 the number of rows in which the user can tile

--- desktop/src/main/scala/org/talkingpuffin/ui/Tile.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/Tile.scala
@@ -9,7 +9,7 @@ object Tile {
   def actions(session: Session, long: Boolean = true) = {
     def rows(num: Int) = " row" + (if (num == 1) "" else "s")
     def title(num: Int) = if (long) "Tile, " + num + rows(num) else "T" + num
-    List.range(1,4).map(num =>
+    List.range(1,6).map(num =>
       new Action(title(num)) {
         toolTip = "Tile windows into " + num + rows(num)
         def apply = eventDistributor.publish(TileViewsEvent(session, num))
