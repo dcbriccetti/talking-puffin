bd0319584c0ff11ba8a4d6e6a9caaafce6eeab09
Dave Briccetti <daveb@davebsoft.com>
2011-02-14 19:48:10 -0800

    Use a symbol to mark retweets. Shorten display of source application (remove “from”).

--- desktop/src/main/scala/org/talkingpuffin/ui/HtmlFormatter.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/HtmlFormatter.scala
@@ -1,6 +1,7 @@
 package org.talkingpuffin.ui
 
 import org.talkingpuffin.util.Loggable
+import org.talkingpuffin.Constants.RetweetSymbol
 import twitter4j.User
 
 /**
@@ -8,8 +9,7 @@ import twitter4j.User
  */
 object HtmlFormatter extends Loggable {
 
-  def createTweetHtml(text: String, replyTo: Option[Long], source: String, 
-                      retweetingUser: Option[User]): String = {
+  def createTweetHtml(text: String, replyTo: Option[Long], source: String, retweetingUser: Option[User]): String = {
     val arrowLinkToParent = LinkExtractor.getReplyToInfo(replyTo, text) match {
       case Some((user, id)) => "<a href='" + LinkExtractor.getStatusUrl(id, user) + "'>↑</a> " 
       case None => ""
@@ -17,9 +17,9 @@ object HtmlFormatter extends Loggable {
               
     val r = LinkExtractor.createLinks(text).replaceAll("\n", "<br/>")
 
-    htmlAround(arrowLinkToParent + fontAround(r, "190%") + 
-        fontAround(" from " + source + 
-        (if (retweetingUser.isDefined) ", RT by " + retweetingUser.get.getScreenName else ""), "80%"))
+    htmlAround(arrowLinkToParent + fontAround(r, "190%") + fontAround(
+        (if (retweetingUser.isDefined) " " + RetweetSymbol + " " + retweetingUser.get.getScreenName else "") +
+          " " + source, "80%"))
   }
   
   def fontAround(s: String, size: String) = 
