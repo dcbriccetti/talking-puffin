de6831701b3d5fe80692efc70f8bc8c4c014a64a
Dave Briccetti <daveb@davebsoft.com>
2011-02-17 00:51:14 -0800

    Extract methods to improve readability

--- desktop/src/main/scala/org/talkingpuffin/twitter/AuthenticatedSession.scala
+++ desktop/src/main/scala/org/talkingpuffin/twitter/AuthenticatedSession.scala
@@ -7,33 +7,13 @@ import twitter4j.{Twitter, TwitterException, TwitterFactory}
 import twitter4j.conf.ConfigurationBuilder
 
 object AuthenticatedSession extends Loggable {
+
   def logIn(credentialsOption: Option[Credentials]): Twitter = {
     val (tw, credentials) = credentialsOption match {
-      case Some(cr) =>
-        (createTwitter(Some(cr)), cr)
-      case None =>
-        val twitter = createTwitter(None)
-        val requestToken = twitter.getOAuthRequestToken
-        DesktopUtil.browse(requestToken.getAuthorizationURL)
-        (twitter, Dialog.showInput(null, "Enter the PIN from the Twitter authorization page in your browser",
-          "Enter PIN", Dialog.Message.Question, null, List[String](), "") match {
-          case Some(pin: String) =>
-            val token = twitter.getOAuthAccessToken(requestToken, pin)
-            CredentialsRepository.save(Credentials(Constants.ServiceName, token.getScreenName,
-                token.getToken, token.getTokenSecret))
-          case _ => throw new RuntimeException("No PIN received")
-        })
+      case Some(cr) => (createTwitter(Some(cr)), cr)
+      case None => createAccount()
     }
-
-    try {
-        val twitterUser = tw.verifyCredentials
-        info("Verified credentials of " + twitterUser.getScreenName)
-    } catch {
-      case te: TwitterException =>
-        CredentialsRepository.delete(credentials)
-        throw new RuntimeException("Failed to verify credentials: " + te.getMessage)
-    }
-
+    verifyCredentials(tw, credentials)
     tw
   }
 
@@ -46,5 +26,33 @@ object AuthenticatedSession extends Loggable {
       cb.setOAuthAccessToken(c.token).setOAuthAccessTokenSecret(c.secret)
     })
     new TwitterFactory(cb.build()).getInstance()
+  }
+
+  private def createAccount(): (Twitter, Credentials) = {
+    val twitter = createTwitter(None)
+    val requestToken = twitter.getOAuthRequestToken
+    DesktopUtil.browse(requestToken.getAuthorizationURL)
+    (twitter, getPin match {
+      case Some(pin: String) =>
+        val token = twitter.getOAuthAccessToken(requestToken, pin)
+        CredentialsRepository.save(Credentials(Constants.ServiceName, token.getScreenName,
+          token.getToken, token.getTokenSecret))
+      case _ => throw new RuntimeException("No PIN received")
+    })
+  }
+
+  private def getPin: Option[String] = Dialog.showInput(null,
+      "Enter the PIN from the Twitter authorization page in your browser",
+      "Enter PIN", Dialog.Message.Question, null, List[String](), "")
+
+  private def verifyCredentials(tw: Twitter, credentials: Credentials): Unit = {
+    try {
+      val twitterUser = tw.verifyCredentials
+      info("Verified credentials of " + twitterUser.getScreenName)
+    } catch {
+      case te: TwitterException =>
+        CredentialsRepository.delete(credentials)
+        throw new RuntimeException("Failed to verify credentials: " + te.getMessage)
+    }
   }
 }
