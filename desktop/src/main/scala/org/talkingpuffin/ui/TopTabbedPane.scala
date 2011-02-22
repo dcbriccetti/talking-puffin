5e946a01cd1eb4c9312d6240e7396f6621487b78
Dave Briccetti <daveb@davebsoft.com>
2011-02-21 17:53:55 -0800

    Set preferred size for the tabbed pane (which controls the size of the whole frame)

--- desktop/src/main/scala/org/talkingpuffin/ui/TopTabbedPane.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/TopTabbedPane.scala
@@ -2,7 +2,9 @@ package org.talkingpuffin.ui
 
 import org.talkingpuffin.Session
 import swing.TabbedPane
+import java.awt.Dimension
 
 class TopTabbedPane(val session: Session) extends TabbedPane {
+  preferredSize = new Dimension(900, 900)
   val tweetDetailPanel = new TweetDetailPanel(session, None)
 }
