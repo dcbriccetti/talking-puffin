a03a18c48bb282416d95cfc5fd5abc94bfe513e5
Dave Briccetti <daveb@davebsoft.com>
2011-03-06 02:01:34 -0800

    Start work on storing Twitter credentials and other user settings in a database

--- web-mvn/src/main/scala/bootstrap/liftweb/Boot.scala
+++ web-mvn/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -4,13 +4,29 @@ import net.liftweb.sitemap.{Menu, SiteMap}
 import net.liftweb.sitemap.Loc._
 import net.liftweb.widgets.flot._
 import net.liftweb.common.{Loggable, Full}
-import net.liftweb.http.{ResourceServer, LiftRules}
 import net.liftweb.widgets.tablesorter.TableSorter
+import _root_.net.liftweb.mapper.{DB, ConnectionManager, Schemifier, DefaultConnectionIdentifier, StandardDBVendor}
 import org.talkingpuffin.snippet.Auth
+import net.liftweb.util.Props
+import org.talkingpuffin.model.User
+import net.liftweb.http.{S, ResourceServer, LiftRules}
 
 class Boot extends Loggable {
   def boot {
+    if (!DB.jndiJdbcConnAvailable_?) {
+      val vendor =
+        new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+          Props.get("db.url") openOr
+            "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+          Props.get("db.user"), Props.get("db.password"))
+
+      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
+
+      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
+    }
+
     LiftRules.addToPackages("org.talkingpuffin")
+    Schemifier.schemify(true, Schemifier.infoF _, User)
 
     val LoggedIn    = If(()     => loggedIn_?, "Not logged in")
     val NotLoggedIn = Unless(() => loggedIn_?, "Logged in")
@@ -39,6 +55,7 @@ class Boot extends Loggable {
     Flot.init
     TableSorter.init
 
+    S.addAround(DB.buildLoanWrapper)
   }
 
   private def loggedIn_? = {
