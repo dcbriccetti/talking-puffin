4d34f96a12b2253e719ce416a06ded3094fe0375
Dave Briccetti <daveb@davebsoft.com>
2011-03-15 23:48:48 -0700

    Use an actor, so a delayed desktop.browse won’t block the UI

--- desktop/src/main/scala/org/talkingpuffin/ui/util/DesktopUtil.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/util/DesktopUtil.scala
@@ -5,8 +5,18 @@ import java.net.URI
 import java.awt._
 import javax.swing.ImageIcon
 import org.talkingpuffin.Main
+import actors.Actor._
 
 object DesktopUtil {
+  private case class Browse(uri: String)
+  val browser = actor {
+    while(true) {
+      receive {
+        case browse: Browse => Desktop.getDesktop.browse(new URI(browse.uri))
+      }
+    }
+  }
+
   val trayIcon: TrayIcon = SystemTray.isSupported match {
     case true =>
       val icon = new TrayIcon(new ImageIcon(getClass.getResource("/TalkingPuffin_16.png")).getImage,Main.title)
@@ -15,7 +25,7 @@ object DesktopUtil {
     case _ => null
   }
 
-  def browse(uri: String) = if (Desktop.isDesktopSupported) Desktop.getDesktop.browse(new URI(uri))
+  def browse(uri: String) = if (Desktop.isDesktopSupported) browser ! Browse(uri)
 
   def notify(message: String, header: String) = if (SystemTray.isSupported) 
     trayIcon.displayMessage(header, message, TrayIcon.MessageType.INFO)
