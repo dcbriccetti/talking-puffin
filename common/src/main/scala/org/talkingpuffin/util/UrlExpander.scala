eccf623fb7efb589f314ea0365bfbb76cb8a8bf6
Dave Briccetti <daveb@davebsoft.com>
2011-03-19 01:07:51 -0700

    Stop looking for redirections when finding the first https protocol. Add time measurement for each URL expansion.

--- common/src/main/scala/org/talkingpuffin/util/UrlExpander.scala
+++ common/src/main/scala/org/talkingpuffin/util/UrlExpander.scala
@@ -1,6 +1,7 @@
 package org.talkingpuffin.util
 
 import java.net.{HttpURLConnection, URL}
+import java.text.NumberFormat
 
 /**
  * URL shortening and expanding.
@@ -9,32 +10,44 @@ object UrlExpander extends Loggable {
   class NoRedirection(msg: String) extends Exception(msg)
   private val redirectionCodes = List(301, 302)
   private val cache = new UrlsCache
+  private val fmt = NumberFormat.getInstance
 
   def expand(urlString: String): String = {
     val url = new URL(urlString)
 
-    if (url.getProtocol == "https")
-      return urlString
+    def process: String = {
+      if (url.getProtocol == "https")
+        return urlString
 
-    cache.get(urlString).foreach(expUrl => return expUrl.url.getOrElse(urlString))
+      cache.get(urlString).foreach(expUrl => return expUrl.url.getOrElse(urlString))
 
-    getRedirectionChain(url) match {
-      case Nil =>
-        debug(urlString + " does not redirect anywhere")
-        cache.put(urlString, None)
-        throw new NoRedirection(urlString)
-      case ultimateUrl :: others =>
-        val expandedUrl = ultimateUrl.toString
-        cache.put(urlString, Some(expandedUrl))
-        expandedUrl
+      getRedirectionChain(url) match {
+        case Nil =>
+          debug(urlString + " does not redirect anywhere")
+          cache.put(urlString, None)
+          throw new NoRedirection(urlString)
+        case ultimateUrl :: others =>
+          val expandedUrl = ultimateUrl.toString
+          cache.put(urlString, Some(expandedUrl))
+          expandedUrl
+      }
     }
+
+    val startTime = System.currentTimeMillis
+    val result = process
+    debug(urlString + " in " + fmt.format(System.currentTimeMillis - startTime) + " ms")
+    result
   }
 
   private def getRedirectionChain(url: URL): List[URL] = {
     val conn = getConnection(url)
-    if (isRedir(conn))
-      getRedirectionChain(new URL(getLocFromHeader(conn, url))) ++ List(url)
-    else
+    if (isRedir(conn)) {
+      val nextUrl = new URL(getLocFromHeader(conn, url))
+      if (nextUrl.getProtocol == "https")
+        nextUrl :: url :: Nil
+      else
+        getRedirectionChain(nextUrl) ++ List(url)
+    } else
       List(url)
   }
 
