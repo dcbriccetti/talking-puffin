35b5f1783f3325523e34ea4e98569fe957e100e8
Dave Briccetti <daveb@davebsoft.com>
2011-03-19 01:02:33 -0700

    Collect and display timing numbers, mean, and standard deviation

--- common/src/main/scala/org/talkingpuffin/util/Parallelizer.scala
+++ common/src/main/scala/org/talkingpuffin/util/Parallelizer.scala
@@ -1,17 +1,40 @@
 package org.talkingpuffin.util
 
+import scala.collection.JavaConversions._
 import java.util.concurrent.{ExecutorCompletionService, Callable, Executors}
+import java.util.{ArrayList, Collections}
+import java.text.NumberFormat
 
-object Parallelizer {
+object Parallelizer extends Loggable {
   /**
    * Runs, in the number of threads requested, the function f, giving it each A of args, returning a List[T]
    */
   def run[T,A](numThreads: Int, args: Seq[A], f: (A) => T): List[T] = {
+    val timings = Collections.synchronizedList(new ArrayList[Long])
     val pool = Executors.newFixedThreadPool(numThreads)
     val completionService = new ExecutorCompletionService[T](pool)
-    args.foreach(arg => completionService.submit(new Callable[T] {def call = f(arg)}))
+    args.foreach(arg => completionService.submit(new Callable[T] {
+      def call = {
+        val startTime = System.currentTimeMillis
+        val result = f(arg)
+        timings.add(System.currentTimeMillis - startTime)
+        result
+      }
+    }))
     val result = args map(_ => completionService.take.get)
     pool.shutdown
+    val fmt = NumberFormat.getInstance
+    val mean = timings.sum.toDouble / timings.size
+    debug(timings.sorted.map(timing => fmt.format(timing)).toList.mkString(", "))
+    debug("Mean: " + fmt.format(mean) + ", Std dev: " + fmt.format(calcStdDev(timings, mean)))
     result.toList
+  }
+
+  private def calcStdDev(timings: java.util.List[Long], mean: Double): Double = {
+    val difSq = timings.map(timing => {
+      val dif = timing - mean
+      dif * dif
+    })
+    math.sqrt(difSq.sum / timings.size)
   }
 }
