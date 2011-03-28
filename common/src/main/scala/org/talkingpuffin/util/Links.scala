08a8b84bf75a738ca6bbe356f44555573eeae172
Dave Briccetti <daveb@davebsoft.com>
2011-03-27 23:46:50 -0700

    Support alternate hosting

--- common/src/main/scala/org/talkingpuffin/util/Links.scala
+++ common/src/main/scala/org/talkingpuffin/util/Links.scala
@@ -1,10 +1,13 @@
 package org.talkingpuffin.util
 
 object Links {
-  def getRedirectUrl(hostName: String, path: String) =
-    (if (hostName == "localhost")
+  def getRedirectUrl(hostName: String, path: String) = (
+    if (hostName == "localhost")
       "http://localhost:8080/"
-    else "http://talkingpuffin.org/tpuf/") + path
+    else if (hostName.contains("vcloudlabs"))
+      "http://tpuf.vcloudlabs.com/"
+    else "http://talkingpuffin.org/tpuf/"
+    ) + path
 
   def linkForAnalyze(screenName: String, hostName: String = System.getProperty("webHost", "talkingpuffin.org")) =
     getRedirectUrl(hostName, "analyze?user=" + screenName)
--- common/src/main/scala/org/talkingpuffin/util/UrlsCache.scala
+++ common/src/main/scala/org/talkingpuffin/util/UrlsCache.scala
@@ -3,7 +3,21 @@ package org.talkingpuffin.util
 import com.redis.RedisClient
 
 class UrlsCache extends Loggable {
-  val redis = new RedisClient
+  val r = """.*"port":(\d+).*"hostname":"(.*?)".*"password":"(.*?)".*""".r
+
+  val redis = Option(System.getenv("VMC_SERVICES")) match {
+    case Some(services) =>
+      info(services)
+      services match {
+        case r(port, hostname, password) => {
+          info("Creating RedisClient for " + hostname + ":" + port)
+          new RedisClient(hostname, Integer.parseInt(port)) { auth(password) }
+        }
+      }
+    case None =>
+      info("Creating RedisClient using defaults")
+      new RedisClient
+  }
 
   def get(urlString: String): Option[String] = {
     val ceu = redis.synchronized { redis.get(urlString) }
