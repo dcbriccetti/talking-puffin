af130b6a6193ecc9eccdfc110698f69ec4a77989
Dave Briccetti <daveb@davebsoft.com>
2011-04-03 14:17:55 -0700

    Switch to Akka actors for DesktopUtil

--- desktop/src/main/scala/org/talkingpuffin/ui/util/DesktopUtil.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/util/DesktopUtil.scala
@@ -4,25 +4,25 @@ import java.awt.Desktop
 import java.net.URI
 import java.awt._
 import javax.swing.ImageIcon
+import akka.actor._
+import akka.actor.Actor._
 import org.talkingpuffin.Main
-import actors.Actor._
 import org.talkingpuffin.util.Loggable
 
 object DesktopUtil extends Loggable {
   private case class Browse(uri: String)
-  val browser = actor {
-    while(true) {
-      receive {
+
+  private val browser = actorOf(new Actor() {
+      def receive = {
         case browse: Browse => {
           info("Before desktop.browse")
           Desktop.getDesktop.browse(new URI(browse.uri))
           info("After desktop.browse")
         }
       }
-    }
-  }
+    }).start()
 
-  val trayIcon: TrayIcon = SystemTray.isSupported match {
+  private val trayIcon: TrayIcon = SystemTray.isSupported match {
     case true =>
       val icon = new TrayIcon(new ImageIcon(getClass.getResource("/TalkingPuffin_16.png")).getImage,Main.title)
       SystemTray.getSystemTray.add(icon)
