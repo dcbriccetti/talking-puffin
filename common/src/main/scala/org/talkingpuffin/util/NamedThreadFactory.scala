4df34171c8be1e17acd3d21609b924fecc677237
Dave Briccetti <daveb@davebsoft.com>
2011-04-02 18:19:16 -0700

    Simplify ID creation

--- common/src/main/scala/org/talkingpuffin/util/NamedThreadFactory.scala
+++ common/src/main/scala/org/talkingpuffin/util/NamedThreadFactory.scala
@@ -1,5 +1,6 @@
 package org.talkingpuffin.util
 import java.util.concurrent.{ThreadFactory, Executors}
+import java.util.concurrent.atomic.AtomicLong
 
 /**
  * A wrapper for the default Executors thread factor that replaces the pool-n-thread-n
@@ -7,11 +8,11 @@ import java.util.concurrent.{ThreadFactory, Executors}
  */
 class NamedThreadFactory(baseName: String) extends ThreadFactory {
   val factory = Executors.defaultThreadFactory
+  val id = new AtomicLong(0)
 
   def newThread(p1: Runnable) = {
     val thread = factory.newThread(p1)
-    val id = thread.getName.split("-").toList.last
-    thread.setName(baseName + " " + id)
+    thread.setName(baseName + " " + id.incrementAndGet)
     thread
   }
 }
