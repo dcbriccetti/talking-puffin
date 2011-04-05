3177a076be9ac5c4e45c65a94f083345ab43b736
Dave Briccetti <daveb@davebsoft.com>
2011-04-04 17:02:17 -0700

    Use Option to simplify logic

--- desktop/src/main/scala/org/talkingpuffin/ui/util/DesktopUtil.scala
+++ desktop/src/main/scala/org/talkingpuffin/ui/util/DesktopUtil.scala
@@ -12,7 +12,7 @@ import org.talkingpuffin.util.Loggable
 object DesktopUtil extends Loggable {
   private case class Browse(uri: String)
 
-  private val browser = actorOf(new Actor() {
+  private lazy val browser = actorOf(new Actor() {
     def receive = {
       case browse: Browse => {
         info("Before desktop.browse")
@@ -22,16 +22,15 @@ object DesktopUtil extends Loggable {
     }
   }).start()
 
-  private val trayIcon: TrayIcon = SystemTray.isSupported match {
-    case true =>
+  private lazy val trayIcon: Option[TrayIcon] =
+    if (SystemTray.isSupported) {
       val icon = new TrayIcon(new ImageIcon(getClass.getResource("/TalkingPuffin_16.png")).getImage,Main.title)
       SystemTray.getSystemTray.add(icon)
-      icon
-    case _ => null
-  }
+      Some(icon)
+    } else None
 
   def browse(uri: String) = if (Desktop.isDesktopSupported) browser ! Browse(uri)
 
-  def notify(message: String, header: String) = if (SystemTray.isSupported) 
-    trayIcon.displayMessage(header, message, TrayIcon.MessageType.INFO)
+  def notify(message: String, header: String) =
+    trayIcon.foreach(_.displayMessage(header, message, TrayIcon.MessageType.INFO))
 }
