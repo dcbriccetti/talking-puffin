079b203d2d24770f98998cbad728126eff7ff240
Dave Briccetti <daveb@davebsoft.com>
2011-10-09 23:51:56 -0700

    Recode the word frequency logic to work in Scala 2.9.1

--- common/src/main/scala/org/talkingpuffin/util/WordCounter.scala
+++ common/src/main/scala/org/talkingpuffin/util/WordCounter.scala
@@ -5,7 +5,7 @@ import io.Source
 case class WordCounter(text: String, wordFilter: String => Boolean = WordCounter.startsWithLetter,
                         wordProcessor: String => String = identity) {
   val words: List[WordCount] = calculateCounts(text)
-  val frequencies: WordCounter.FreqToStringsMap = calculateBuckets(words)
+  val frequencies: WordCounter.FreqToStringsMap = words.groupBy(_.count).mapValues(_.map(wc => wc.word))
 
   case class WordCount(word: String, count: Long) {
     def +(n: Long): WordCount = WordCount(word, count + n)
@@ -16,11 +16,14 @@ case class WordCounter(text: String, wordFilter: String => Boolean = WordCounter
         withFilter(w => w.trim.length > 0 && ! w.toLowerCase.startsWith("http") &&
                    ! WordCounter.stopList.contains(w) && wordFilter(w)).
         map(dropTrailingPunctuation).map(wordProcessor)
-    val emptyMap = Map.empty[String, WordCount].withDefault(w => WordCount(w, 0))
-    val countsMap = words.foldLeft(emptyMap) {(map, word) =>
-      map(word) += 1
-    }
-    countsMap.values.toList.sort(_.count > _.count)
+    val countsMap = scala.collection.mutable.Map[String, WordCount]()
+    words.foreach(word => {
+      countsMap.get(word) match {
+        case Some(wc) => countsMap(word) = wc + 1
+        case _ => countsMap(word) = WordCount(word, 0)
+      }
+    })
+    countsMap.values.toList.sortBy(_.count)
   }
 
   private def dropTrailingPunctuation(word: String) =
@@ -29,12 +32,6 @@ case class WordCounter(text: String, wordFilter: String => Boolean = WordCounter
       case w => w
     }
 
-  private def calculateBuckets(wordCounts: List[WordCount]): WordCounter.FreqToStringsMap = {
-    val emptyMap = Map.empty[Long,List[String]].withDefaultValue(List[String]())
-    wordCounts.foldLeft(emptyMap) {(map, wordCount) =>
-        map(wordCount.count) = wordCount.word :: map(wordCount.count)
-    }
-  }
 }
 
 object WordCounter {
@@ -44,5 +41,5 @@ object WordCounter {
   def startsWithLetter(word: String) = word(0).isLetter
 
   private val stopList: Set[String] = Source.fromInputStream(
-    getClass.getResourceAsStream("/stoplist.csv")).getLines.mkString(",").split(",").toSet
+    getClass.getResourceAsStream("/stoplist.csv")).getLines().mkString(",").split(",").toSet
 }
