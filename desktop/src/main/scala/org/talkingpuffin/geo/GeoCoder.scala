b89ee3ee8a64b74b9ca70ccfdef08b4d08567789
Dave Briccetti <daveb@davebsoft.com>
2012-08-06 19:58:52 -0700

    Handle geocoding from the iPhone: <location>

--- desktop/src/main/scala/org/talkingpuffin/geo/GeoCoder.scala
+++ desktop/src/main/scala/org/talkingpuffin/geo/GeoCoder.scala
@@ -1,28 +1,39 @@
 package org.talkingpuffin.geo
 
 import java.net.URL
-import _root_.scala.xml.XML
-import com.google.common.collect.MapMaker
-import org.talkingpuffin.util.BackgroundResourceFetcher
+import xml.XML
+import org.talkingpuffin.util.{Loggable, BackgroundResourceFetcher}
 
 /**
  * Geocoding.
  */
-object GeoCoder extends BackgroundResourceFetcher[String]("Geo", numThreads = 2) {
-  private val locationCache: java.util.Map[String, String] = new MapMaker().softValues().makeMap()
-  private val num = """(-?\d+\.\d*)"""
-  private val latLongRegex = ("""[^-\d]*""" + num + """,\s*""" + num).r
+object GeoCoder extends BackgroundResourceFetcher[String]("Geo", numThreads = 2) with Loggable {
+  private val latLongRegex = {
+    val num = """(-?\d+\.\d*)"""
+    val latLongRexexSource = num + """,\s*""" + num
+    info(latLongRexexSource)
+    latLongRexexSource.r
+  }
   private val apiKey = "ABQIAAAAVOsftmRci5v5FgKzeSDTjRQRy7BtqlAMzRCsHHZRQCk8HnV1mBQ5tPe8d9oZTkHqFfsayPz758T-Mw"
 
   /**
    * From a (latitude, comma, optional spaces, longitude) alone or contained inside other characters, 
    * produces a (latitude, comma, longitude) String, or None if the pattern does not match.
    */
-  def extractLatLong(location: String): Option[String] = location match {
-    case latLongRegex(lat, long) => Some(formatLatLongKey(lat, long))
-    case _ => None
+  def extractLatLong(location: String): Option[String] = {
+    info(location)
+    val lr = location.replaceAll(".*: ?", "")
+    info(lr)
+    lr match {
+      case latLongRegex(lat, long) =>
+        info("match %s %s".format(lat,long))
+        Some(formatLatLongKey(lat, long))
+      case _ =>
+        info("no match")
+        None
+    }
   }
-  
+
   def formatLatLongKey(lat: String, long: String): String = lat + "," + long 
   
   def formatLatLongKey(location: Tuple2[Double, Double]): String = 
@@ -34,5 +45,4 @@ object GeoCoder extends BackgroundResourceFetcher[String]("Geo", numThreads = 2)
     (XML.load(url.openConnection.getInputStream) \ "Response" \ "Placemark" \ "address").
         headOption.map(_.text).getOrElse(latLong)
   }
-  
 }
